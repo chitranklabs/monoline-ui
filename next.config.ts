1c444ca03da422169e45d25f36af1436ecf0c824
chitrank2050 <chitrank2050@gmail.com>
2026-06-08 10:31:12 +0530

    feat: enable React Compiler and optimize package imports in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,21 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+	trailingSlash: false,
 	transpilePackages: ["@chitrank2050/monoline-ui"],
+	reactCompiler: true,
+	poweredByHeader: false,
+	compress: true,
 	experimental: {
-		optimizePackageImports: ["@chitrank2050/monoline-ui"],
+		optimizePackageImports: [
+			"@chitrank2050/monoline-ui",
+			"@radix-ui/react-slot",
+			"cmdk",
+			"prismjs",
+			"@vercel/analytics",
+			"clsx",
+			"tailwind-merge",
+		],
 	},
 }
 
