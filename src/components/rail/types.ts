e967d975bd0b5c04a821fbd20a4b055bd9367823
chitrank2050 <chitrank2050@gmail.com>
2026-06-26 18:37:36 +0530

    refactor: omit title from RailProps component properties to avoid HTML attribute conflicts

--- src/components/rail/types.ts
+++ src/components/rail/types.ts
@@ -1,6 +1,6 @@
 import type * as React from "react"
 
-export interface RailProps extends React.ComponentProps<"div"> {
+export interface RailProps extends Omit<React.ComponentProps<"div">, "title"> {
 	title?: React.ReactNode
 }
 
